eef4e8904835ecd9074b7e06cc88f3550d5df7f8
Mark Arnold <mark.arnold@accesshq.com>
2021-09-29 15:36:16 +1000

    Add TableQtyTest, MovingButtonTest

--- src/test/java/com/accesshq/tests/WebTestSuite.java
+++ src/test/java/com/accesshq/tests/WebTestSuite.java
@@ -1,24 +1,64 @@
 package com.accesshq.tests;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
+
+import java.util.List;
 
 public class WebTestSuite {
 
+    WebDriver driver;
+
+    @BeforeEach
+    public void SetUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
+    }
+
     @Test
     public void DemoTest() {
-        // Arrange
-        var driver = new ChromeDriver();
-        driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
-
         // Act
         var actualText = driver.findElement(By.cssSelector("h1.display-1")).getText();
 
         // Assert
         Assertions.assertEquals("Web Playground", actualText);
 
+        driver.quit();
+    }
+
+    @Test
+    public void TableQuantityTest() {
+        // Act
+        var tableElement = driver.findElement(By.tagName("table"));
+        List<WebElement> inputElements = tableElement.findElements(By.tagName("input"));
+
+        // Assert
+        Assertions.assertEquals(5, Integer.parseInt(inputElements.get(0).getAttribute("value")));
+    }
+
+    @Test
+    public void MovingButtonTest() {
+        // Arrange
+
+        // Act
+        var upDownButton = getUpDownButton();
+        upDownButton.click();
+
+        // Assert
+        String expected = "CLICK ME UP!";
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBePresentInElement(upDownButton, expected));
+        Assertions.assertEquals(expected, upDownButton.getText());
+    }
+
+    private WebElement getUpDownButton() {
+        return driver.findElement(By.cssSelector("a[role=button]"));
+    }
+
+    @AfterEach
+    public void Cleanup() {
         driver.quit();
     }
 }
