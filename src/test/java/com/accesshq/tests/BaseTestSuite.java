d87e38e572b9b255bccf7a2e217cb30e01c5639b
Mark Arnold <mark.arnold@accesshq.com>
2021-10-04 13:08:40 +1000

    Update framework to run tests in parallel

--- src/test/java/com/accesshq/tests/BaseTestSuite.java
+++ src/test/java/com/accesshq/tests/BaseTestSuite.java
@@ -3,14 +3,20 @@ package com.accesshq.tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTestSuite {
-    protected ChromeDriver driver;
+    protected WebDriver driver;
 
     @BeforeEach
-    public void setUp() {
-        driver = new ChromeDriver();
+    public void setUp() throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), new ChromeOptions());
         //driver.manage().window().setPosition(new Point(0, -1000));
         driver.manage().window().maximize();
         driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
